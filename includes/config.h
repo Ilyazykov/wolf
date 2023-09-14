12f01a50b957f16f148d9c19350af3f3b97ac21e
Ilya Zykov <ilya.zykov@icloud.com>
2023-09-14 22:07:37 +0200

    [main] add character and config

---
+++ includes/config.h
@@ -0,0 +1,3 @@
+const char WALL = '#';
+const char CHARACTER = 'c';
+const char SPACE = '_';
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+
 #include "map.h"
+#include "character.h"
+#include "config.h"
 
 int main() {
     Map map("maps/1.map");
+    Character ch({1,1}, {0,1}, 0);
 
     std::cout << map << std::endl;
 
